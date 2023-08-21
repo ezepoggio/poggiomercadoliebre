1fc021b93da67609c76c818bcb92e08e1044a47b
ezepoggio <poggioezequieln@gmail.com>
2023-08-21 20:44:25 -0300

    Proyecto actualizado

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Se prendio en el puerto ${PORT}`);;
+    console.log(`Se prendio en el puerto ${PORT}`);
 });
 
 app.get('/', (req, res) => {
